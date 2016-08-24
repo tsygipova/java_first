3a130f6a7103783b335800433fdd724775a40569
Dariia <tsygipova@gmail.com>
2016-08-24 21:29:11 +0300

    Метод перенесен в класс, добавлен конструктор

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,4 +7,29 @@ public class Point {
 
   public double x;
   public double y;
+
+  public Point(double x,double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public static void main(String args[]) {
+    Point p1 = new Point(0.5, 7.32);
+    Point p2 = new Point(8.9, 4);
+    System.out.println("p1 = " + p1.x + ", " + p1.y);
+    System.out.println("p2 = " + p2.x + ", " + p2.y);
+
+  }
+  public double distance(Point p) {
+    double dx = this.x - p.x;
+    double dy = this.y - p.y;
+    return Math.sqrt(dx*dx + dy*dy);
+
+  }
+
+  public double distance(Point p) {
+    return distance(p.x, p.y);
+  }
 }
+
+//class PointDist {
