abaffd78bc133a1086431d744821f5ea65cd291b
Dariia <tsygipova@gmail.com>
2016-08-24 21:29:40 +0300

    Перенос метода к класс

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,20 +2,30 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
-    Point p1 = new Point();
-    p1.x = 0.5;
-    p1.y = 7.32;
-    Point p2 = new Point();
-    p2.x = 8.9;
-    p2.y = 4;
-    System.out.println("p1 = " + p1.x + "," + p1.y);
-    System.out.println("p2 = " + p2.x + "," + p2.y);
-    System.out.println("Расстояние между двумя точками на плоскости" + " = " + distance(p1,p2));
+//  public static void main(String[] args) {
+//    hello("world");
+//    hello("user");
+//    hello("Dariia");
+//
+//    Square s = new Square(5);
+//    System.out.println("Площадь квадрата со стороной" + s.l + "=" + s.area());
+//
+//    Rectangle r = new Rectangle(4, 6);
+//    System.out.println("Площадь прямоугольника со сторонами" + r.a + "и" + r.b + "=" + s.area());
+//  }
+
+    Point p1 = new Point(0.5, 7.32);
+    Point p2 = new Point(8.9, 4);
+    System.out.println("Расстояние между двумя точками на плоскости" + " = " + distance(p1, p2));
 
   }
 
-  public static double distance(Point p1, Point p2) {
-   return Math.sqrt(Math.pow(p2.x - p1.x,2) + Math.pow(p2.y-p1.y,2));
+
+//  public static double distance(Point p1, Point p2) {
+//   return Math.sqrt(Math.pow(p2.x - p1.x,2) + Math.pow(p2.y-p1.y,2));
+//  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
   }
 }
