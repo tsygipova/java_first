1c01e42c9cd87eb13e0f5b317d10c90eaf46e1f1
Dariia <tsygipova@gmail.com>
2016-10-16 14:50:55 +0300

    Модификация контактов и групп - реализация путем сравнения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactsModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactsModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactsData;
 
@@ -10,9 +11,17 @@ public class ContactsModificationTests extends TestBase {
 
   @Test
   public void testContactsModification(){
+    int before = app.getContactsHelper().getContactCount();
+    if (! app.getContactsHelper().isThereAContact()) {
+      app.getContactsHelper().createContact(new ContactsData("test1", null, null, null, null, null, null, null, null));
+    }
     app.getContactsHelper().initContactModification();
+    app.getContactsHelper().fillContactForm(new ContactsData("test1", "test2", "test3", "test4", "test5", "098765", "46576576767", "mail1@mail.com", null));
     app.getContactsHelper().submitContactModification();
-    app.returnToContactsPage();
+    app.getContactsHelper().returnToHomePage();
+    int after = app.getContactsHelper().getContactCount();
+    Assert.assertEquals(after, before);
+
   }
 
 }
