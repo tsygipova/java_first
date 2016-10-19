d3af03240fc007d739864bc66b872981160b5358
Dariia <tsygipova@gmail.com>
2016-10-19 15:41:23 +0300

    Тесты для расстояния между точками. задание 3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,18 +1,17 @@
-//package ru.stqa.pft.sandbox;
-//
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
-//
-///**
-// * Created by Дарья on 26-Aug-16.
-// */
-//public class PointTests {
-//
-//  @Test
-//  public void testDistance() {
-//    Point p1 = new Point(0.5, 7.32);
-//    Point p2 = new Point(8.9, 4);
-//    Assert.assertEquals(p1.distance(), 9.08);
-//
-//  }
-//}
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Дарья on 26-Aug-16.
+ */
+public class PointTests {
+
+  @Test
+  public void testDistance1() {
+    Point p1 = new Point(10, 5);
+    Point p2 = new Point(20, 5);
+    assert p1.distance(p2) == 10;
+  }
+}
